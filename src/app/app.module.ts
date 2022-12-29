d6519f881cb696a21db69264954e2878e7226f0c
LeandroMoyano <108894094+LeandroMoyano@users.noreply.github.com>
2022-12-29 20:29:42 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutSkillsComponent } from './componentes/about-skills/about-skills.component';
+import { ContactComponent } from './componentes/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutSkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
